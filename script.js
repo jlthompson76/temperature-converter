43b39b4b208432fdd5bc93d94e8800fa6670f30d
Jamie Thompson <jlthompson76@users.noreply.github.com>
2020-03-13 22:37:42 -0400

    Add stylesheet, modify index and script files, move code from CodeSquad instructors into new subdirectory

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-const name = prompt("What is your name?");
+function tempConverter() {
 
-const fahrenheit = prompt(`Hi, ${name}, please enter the .temperature in Fahrenheit and I will convert it into Celsius for you`);
-const celsius = toCelsius(fahrenheit);
-displayConversion(fahrenheit);
+    const name = prompt("What is your name?");
 
-function toCelsius(fahrenheit) {
-    return (fahrenheit - 32) * (5 / 9);
-}
+    const fahrenheit = prompt(`Hi, ${name}, please enter the temperature in Fahrenheit and I will convert it into Celsius for you!`);
+    const celsius = toCelsius(fahrenheit);
+    displayConversion(fahrenheit);
 
-function displayConversion(fahrenheit) {
-    let conversion = `${fahrenheit}\xB0 Fahrenheit is ${celsius}\xB0 Celsius`;
-    alert(conversion);
-    let  message;
-    if (fahrenheit <= 69) {
-        message = `It's cold outside, ${name}! Don't forget your coat!`;
-    } else if (fahrenheit >= 84) {
-       message = `It's hot outside, ${name}! Don't forget your sunscreen!`;
-    } else {
-        message = `It's perfect weather, ${name}, enjoy!`;
+    function toCelsius(fahrenheit) {
+        return (fahrenheit - 32) * (5 / 9);
     }
-    alert(message);
-  }
+
+    function displayConversion(fahrenheit) {
+        let conversion = `${fahrenheit}\xB0 Fahrenheit is ${celsius}\xB0 Celsius`;
+        alert(conversion);
+        let  message;
+        if (fahrenheit <= 69) {
+            message = `It's cold outside, ${name}! Don't forget your coat!`;
+        } else if (fahrenheit >= 84) {
+        message = `It's hot outside, ${name}! Don't forget your sunscreen!`;
+        } else {
+            message = `It's perfect weather, ${name}, enjoy!`;
+        }
+        alert(message);
+    }
+    
+}
